44470762c97f4cab7222401c14d8c355326b31fc
Sergio5714 <sergey57145714@mail.ru>
2017-11-03 19:07:34 +0300

    Some code's style changes in main.c

--- main.c
+++ main.c
@@ -27,8 +27,8 @@ float Temperature = 0;
 //Prepare Array in "fucking ASCII format" (FASCII)
 void PrepareAndSendPackageInFASCII(USART_TypeDef * USARTx, char*array, int temperature, int humidity, int co2Concentration)
 {
-	 int i = 0;
-			//convert integer to character
+	int i = 0;
+	//convert integer to character
   do
   {
 		array[i++] = 0;
@@ -39,7 +39,7 @@ void PrepareAndSendPackageInFASCII(USART_TypeDef * USARTx, char*array, int tempe
 	array[i++] = 0;
 	array[i++] = ',';
 	
-	  do
+	do
   {
 		array[i++] = 0;
     array[i++] = (char)( humidity% 10) + '0';
@@ -49,7 +49,7 @@ void PrepareAndSendPackageInFASCII(USART_TypeDef * USARTx, char*array, int tempe
 	array[i++] = 0;
 	array[i++] = ',';	
 	
-		do
+	do
   {
 		array[i++] = 0;
     array[i++] = (char)( temperature% 10) + '0';
@@ -59,16 +59,16 @@ void PrepareAndSendPackageInFASCII(USART_TypeDef * USARTx, char*array, int tempe
   //send data
   while(i)
   {
-	UsartPutC(USARTx, array[--i]); 
+		UsartPutC(USARTx, array[--i]); 
   }
 }
 void USART1_IRQHandler (void)
 {
 	if(USART1->SR & USART_SR_RXNE) // if Read buffer not empty flag is set
-			{
-				//Clear flag
-				USART1->SR &= ~USART_SR_RXNE;
-			}
+	{
+		//Clear flag
+		USART1->SR &= ~USART_SR_RXNE;
+	}
 }
 void massShift( char* massPointer, int size)
 {
@@ -109,130 +109,133 @@ int recognizeString(const char* sequence, int sizeOfSequence, char* massPointer,
 void USART2_IRQHandler (void)
 {
 	if(USART2->SR & USART_SR_RXNE) // if Read buffer not empty flag is set
-			{
-				massShift(&inputBuffer[0], sizeOfInputBuffer);
-				inputBuffer[0] = USART2->DR;
-				messageReceived = 1;
-				//Clear flag
-				USART2->SR&=~USART_SR_RXNE;
-			}
+	{
+		massShift(&inputBuffer[0], sizeOfInputBuffer);
+		inputBuffer[0] = USART2->DR;
+		messageReceived = 1;
+		//Clear flag
+		USART2->SR&=~USART_SR_RXNE;
+	}
 }
 void TIM2_IRQHandler (void)
 {
-		if(TIM2->SR & TIM_SR_UIF) // if UIF flag is set
-			{
-				TIM2->SR &= ~TIM_SR_UIF; // clear UIF flag
-			}
-		//PrepareAndSendPackageInFASCII(USART2, &outputBuffer[0], 27, 64, 400);
-		PrepareAndSendPackageInFASCII(USART2, &outputBuffer[0], Temperature, Humidity, CO2Concentration);
+	if(TIM2->SR & TIM_SR_UIF) // if UIF flag is set
+	{
+		TIM2->SR &= ~TIM_SR_UIF; // clear UIF flag
+	}
+	//PrepareAndSendPackageInFASCII(USART2, &outputBuffer[0], 27, 64, 400);
+	PrepareAndSendPackageInFASCII(USART2, &outputBuffer[0], Temperature, Humidity, CO2Concentration);
 }
 void ConfigureBLEMaster()
 {
 	while (1)
+	{
+		UsartPutS(USART2, "AT");
+		TIMDelay(TIM3);
+		if (recognizeString("OK", 2, &inputBuffer[0], sizeOfInputBuffer))
 		{
-			UsartPutS(USART2, "AT");
-			TIMDelay(TIM3);
-			if (recognizeString("OK", 2, &inputBuffer[0], sizeOfInputBuffer))
-			{
-				massClear(&inputBuffer[0], sizeOfInputBuffer);
-				break;
-			}
+			massClear(&inputBuffer[0], sizeOfInputBuffer);
+			break;
 		}
+	}
 	while (1)
+	{
+		UsartPutS(USART2, "AT+IMME1");
+		TIMDelay(TIM3);
+		if (recognizeString("OK", 2, &inputBuffer[0], sizeOfInputBuffer))
 		{
-			UsartPutS(USART2, "AT+IMME1");
-			TIMDelay(TIM3);
-			if (recognizeString("OK", 2, &inputBuffer[0], sizeOfInputBuffer))
-			{
-				massClear(&inputBuffer[0], sizeOfInputBuffer);
-				break;
-			}
+			massClear(&inputBuffer[0], sizeOfInputBuffer);
+			break;
 		}
+	}
 	while (1)
+	{
+		UsartPutS(USART2, "AT+ROLE1");
+		TIMDelay(TIM3);
+		if (recognizeString("OK", 2, &inputBuffer[0], sizeOfInputBuffer))
 		{
-			UsartPutS(USART2, "AT+ROLE1");
-			TIMDelay(TIM3);
-			if (recognizeString("OK", 2, &inputBuffer[0], sizeOfInputBuffer))
-			{
-				massClear(&inputBuffer[0], sizeOfInputBuffer);
-				break;
-			}
+			massClear(&inputBuffer[0], sizeOfInputBuffer);
+			break;
 		}
+	}
 	while (1)
+	{
+		UsartPutS(USART2, "AT+COND43639DAE426");
+		TIMDelay(TIM3);
+		if (recognizeString("OK+CONNA", 2, &inputBuffer[0], sizeOfInputBuffer))
 		{
-			UsartPutS(USART2, "AT+COND43639DAE426");
-			TIMDelay(TIM3);
-			if (recognizeString("OK+CONNA", 2, &inputBuffer[0], sizeOfInputBuffer))
-			{
-				massClear(&inputBuffer[0], sizeOfInputBuffer);
-				break;
-			}
+			massClear(&inputBuffer[0], sizeOfInputBuffer);
+			break;
 		}
+	}
 }
 void ConfigureBLESlave()
 {
-		while (1)
-		{
-			UsartPutS(USART2, "AT+RENEW");
-			TIMDelay(TIM3);
-			if (recognizeString("OK", 2, &inputBuffer[0], sizeOfInputBuffer))
-			{
-				massClear(&inputBuffer[0], sizeOfInputBuffer);
-				break;
-			}
-		}
 	while (1)
+	{
+		UsartPutS(USART2, "AT+RENEW");
+		TIMDelay(TIM3);
+		if (recognizeString("OK", 2, &inputBuffer[0], sizeOfInputBuffer))
 		{
-			UsartPutS(USART2, "AT");
-			TIMDelay(TIM3);
-			if (recognizeString("OK", 2, &inputBuffer[0], sizeOfInputBuffer))
-			{
-				massClear(&inputBuffer[0], sizeOfInputBuffer);
-				break;
-			}
+			massClear(&inputBuffer[0], sizeOfInputBuffer);
+			break;
 		}
+	}
 	while (1)
+	{
+		UsartPutS(USART2, "AT");
+		TIMDelay(TIM3);
+		if (recognizeString("OK", 2, &inputBuffer[0], sizeOfInputBuffer))
 		{
-			UsartPutS(USART2, "AT+ROLE0");
-			TIMDelay(TIM3);
-			if (recognizeString("OK", 2, &inputBuffer[0], sizeOfInputBuffer))
-			{
-				massClear(&inputBuffer[0], sizeOfInputBuffer);
-				break;
-			}
+			massClear(&inputBuffer[0], sizeOfInputBuffer);
+			break;
 		}
+	}
 	while (1)
+	{
+		UsartPutS(USART2, "AT+ROLE0");
+		TIMDelay(TIM3);
+		if (recognizeString("OK", 2, &inputBuffer[0], sizeOfInputBuffer))
 		{
-			UsartPutS(USART2, "AT+RESET");
-			TIMDelay(TIM3);
-			if (recognizeString("OK", 2, &inputBuffer[0], sizeOfInputBuffer))
-			{
-				massClear(&inputBuffer[0], sizeOfInputBuffer);
-				break;
-			}
+			massClear(&inputBuffer[0], sizeOfInputBuffer);
+			break;
 		}
+	}
+	while (1)
+	{
+		UsartPutS(USART2, "AT+RESET");
+		TIMDelay(TIM3);
+		if (recognizeString("OK", 2, &inputBuffer[0], sizeOfInputBuffer))
+		{
+			massClear(&inputBuffer[0], sizeOfInputBuffer);
+			break;
+		}
+	}
 }
 int main()
 {
 	SystemCoreClockUpdate();
-
 	
 	//Timer for sending all the data to Bluetooth HM-10 module
-	TIMInit(TIM2, 35999, 2000);//1 seconds
+	TIMInit(TIM2, 35999, 2000);	//1 second
+	
 	//delay timer
 	TIMInit(TIM3, 35999, 1000);//1
 	TIMInit(TIM4, 35999, 20000);// 10 seconds
+	
 	__enable_irq();
 	NVIC_EnableIRQ(USART2_IRQn);
 	//NVIC_EnableIRQ(USART1_IRQn);
 	NVIC_EnableIRQ(TIM2_IRQn);
 	//NVIC_EnableIRQ(TIM3_IRQn);
 	
-	//Possible solution to launch mhZ19
+	//Possible solution to launch mhZ19 (it nedds some time to be initialized)
 	TIMDelay(TIM4);
 
-	UsartInit(USART2,9600);//for BLE 4.0
-	UsartInit(USART1,9600);//for CO2 sensor
+	//for BLE 4.0
+	UsartInit(USART2,9600);
+	//for CO2 sensor
+	UsartInit(USART1,9600);
 	
 	ConfigureBLESlave();
 	
@@ -246,56 +249,57 @@ int main()
 	TIMDelay(TIM3);
 	
 	TIMEnable(TIM2);
-		while(1)
+	while(1)
 	{
 		if(Z19MeasureCO2(USART1, &CO2Concentration) == ERROR)
-			{
-				UsartDisable(USART1);
-				UsartInit(USART1, 9600);
-				debugMarker++;
-			}
+		{
+			UsartDisable(USART1);
+			UsartInit(USART1, 9600);
+			debugMarker++;
+		}
 		if(Si7021MeasureTemperature(&Temperature) == ERROR)
-			{
-				I2CReset();
-				Si7021Reset();
-			}
+		{
+			I2CReset();
+			Si7021Reset();
+		}
 		if (Si7021MeasureHumidity(&Humidity) == ERROR)
-			{
-				I2CReset();
-				Si7021Reset();
-			}
+		{
+			I2CReset();
+			Si7021Reset();
+		}
 		if (mode == 0)
-			{
-			}
+		{
+		// Some action
+		}
 		if (mode == 1)
+		{
+			if (CO2Concentration > 1000)
 			{
-				if (CO2Concentration > 1000)
-					{
-						UsartPutC(USART2, 1);
-					}
-				if (CO2Concentration < 1000)
-					{
-						UsartPutC(USART2, 0);
-					}
+				UsartPutC(USART2, 1);
 			}
+			if (CO2Concentration < 1000)
+			{
+				UsartPutC(USART2, 0);
+			}
+		}
 		if (messageReceived == 1)
+		{
+			if (recognizeString("111", 2, &inputBuffer[0], sizeOfInputBuffer))
 			{
-				if (recognizeString("111", 2, &inputBuffer[0], sizeOfInputBuffer))
-					{
-						mode = 1;
-						massClear(&inputBuffer[0], sizeOfInputBuffer);
-						TIMDisable(TIM2);
-						ConfigureBLEMaster();
-					}
-				if (recognizeString("000", 2, &inputBuffer[0], sizeOfInputBuffer))
-					{
-						mode = 0;
-						massClear(&inputBuffer[0], sizeOfInputBuffer);
-						ConfigureBLESlave();
-						TIMEnable(TIM2);
-					}
-				messageReceived = 0;
+				mode = 1;
+				massClear(&inputBuffer[0], sizeOfInputBuffer);
+				TIMDisable(TIM2);
+				ConfigureBLEMaster();
 			}
+			if (recognizeString("000", 2, &inputBuffer[0], sizeOfInputBuffer))
+			{
+				mode = 0;
+				massClear(&inputBuffer[0], sizeOfInputBuffer);
+				ConfigureBLESlave();
+				TIMEnable(TIM2);
+			}
+			messageReceived = 0;
+		}
 		TIMDelay(TIM4);
 	}
 }
